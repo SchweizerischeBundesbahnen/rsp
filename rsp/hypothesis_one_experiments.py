1ed5ae80c11cc81c941e44fc2693856b87c0a5ee
u214892 <u214892@sbb.ch>
2019-12-17 21:42:08 +0100

    SIM-155 temporarily disabled verification for full and delta re-scheduling

--- rsp/hypothesis_one_experiments.py
+++ rsp/hypothesis_one_experiments.py
@@ -1,6 +1,6 @@
 from rsp.asp.asp_experiment_solver import ASPExperimentSolver
 from rsp.utils.data_types import ParameterRanges
-from rsp.utils.experiments import create_experiment_agenda
+from rsp.utils.experiments import create_experiment_agenda, run_specific_experiments_from_research_agenda
 from rsp.utils.experiments import run_experiment_agenda
 
 if __name__ == '__main__':
@@ -14,10 +14,10 @@ if __name__ == '__main__':
                                        malfunction_duration=[20, 20, 1])
 
     # Define the desired speed profiles
-    speed_data = {1.: 1./3.,  # Fast passenger train
-                  1. / 2.: 1./3.,  # Fast freight train
+    speed_data = {1.: 1. / 3.,  # Fast passenger train
+                  1. / 2.: 1. / 3.,  # Fast freight train
                   1. / 3.: 0.,  # Slow commuter train
-                  1. / 4.: 1./3.}  # Slow freight train
+                  1. / 4.: 1. / 3.}  # Slow freight train
 
     # Create an experiment agenda out of the parameter ranges
     experiment_agenda = create_experiment_agenda(experiment_name="exp_hypothesis_one",
@@ -29,5 +29,5 @@ if __name__ == '__main__':
     solver = ASPExperimentSolver()
 
     # Run experiments
-    run_experiment_agenda(solver, experiment_agenda, run_experiments_parallel=True, show_results_without_details=True,
+    run_experiment_agenda(solver, experiment_agenda, run_experiments_parallel=False, show_results_without_details=True,
                           verbose=True)
