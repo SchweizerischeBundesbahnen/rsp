7f4e254cc768753488d7b90d3645c1adbc91546d
u229589 <christian.baumberger@sbb.ch>
2020-03-09 10:59:57 +0100

    refactor methods intor separate file

--- tests/test_encounter_graph.py
+++ tests/test_encounter_graph.py
@@ -1,121 +1,20 @@
 from typing import List
 
-import matplotlib.pyplot as plt
-import networkx as nx
 import numpy as np
-from flatland.envs.rail_trainrun_data_structures import Trainrun
 from flatland.envs.rail_trainrun_data_structures import TrainrunDict
 
 from rsp.experiment_solvers.experiment_solver import asp_schedule_wrapper
 from rsp.route_dag.generators.route_dag_generator_schedule import schedule_problem_description_from_rail_env
 from rsp.utils.data_types import ExperimentParameters
 from rsp.utils.data_types import ExperimentResultsAnalysis
-from rsp.utils.data_types import TrainSchedule
-from rsp.utils.data_types import TrainScheduleDict
+from rsp.utils.encounter_graph import compute_undirected_distance_matrix
+from rsp.utils.encounter_graph import plot_encounter_graph_undirected
 from rsp.utils.experiments import create_env_pair_for_experiment
 from rsp.utils.experiments import load_and_expand_experiment_results_from_folder
 from rsp.utils.flatland_replay_utils import convert_trainrundict_to_entering_positions_for_all_timesteps
 
 
-def distance_between_trains(train_schedule_0: TrainSchedule, train_run_0: Trainrun,
-                            train_schedule_1: TrainSchedule, train_run_1: Trainrun) -> (float, int):
-    train_0_start_time = train_run_0[0].scheduled_at
-    train_0_end_time = train_run_0[-1].scheduled_at
-    train_1_start_time = train_run_1[0].scheduled_at
-    train_1_end_time = train_run_1[-1].scheduled_at
-
-    # if the time window of the two trains do not overlap -> no relationship between trains
-    if train_0_end_time < train_1_start_time or train_1_end_time < train_0_start_time:
-        return 0, 0
-
-    # some timesteps overlap -> find out which ones
-    if train_0_start_time <= train_1_start_time:
-        start_time_step = train_1_start_time
-    else:
-        start_time_step = train_0_start_time
-
-    if train_0_end_time <= train_1_end_time:
-        end_time_step = train_0_end_time
-    else:
-        end_time_step = train_1_end_time
-
-    # get positions in time window
-    train_0_positions = [waypoint.position for i, waypoint in train_schedule_0.items() if
-                         (start_time_step <= i <= end_time_step)]
-    train_1_positions = [waypoint.position for i, waypoint in train_schedule_1.items() if
-                         (start_time_step <= i <= end_time_step)]
-
-    # compute distances of positions in time window
-    distances_in_time_window = np.zeros((1, len(train_0_positions)))
-    for i, pos in enumerate(train_0_positions):
-        distances_in_time_window[0, i] = np.sqrt(
-            np.sum(np.square([pos[0] - train_1_positions[i][0], pos[1] - train_1_positions[i][1]])))
-
-    # first heuristic -> get the smallest distance
-    # todo: try different features
-    dist_between_trains = np.min(distances_in_time_window)
-
-    return 1. / dist_between_trains, len(train_0_positions)
-
-
-def compute_distance_matrix(trainrun_dict: TrainrunDict, train_schedule_dict: TrainScheduleDict) -> np.ndarray:
-    number_of_trains = len(trainrun_dict)
-    distance_matrix = np.zeros((number_of_trains, number_of_trains))
-
-    for row, train_schedule_row in train_schedule_dict.items():
-        for column, train_schedule_column in train_schedule_dict.items():
-            if column > row:
-                train_run_row = trainrun_dict.get(row)
-                train_run_column = trainrun_dict.get(column)
-                distance, overlapping_length = distance_between_trains(train_schedule_row, train_run_row,
-                                                                       train_schedule_column, train_run_column)
-                distance_matrix[row, column] = distance
-                distance_matrix[column, row] = distance
-
-    return distance_matrix
-
-
-def plot_encounter_graph(distance_matrix: np.ndarray, title: str, pos: dict = None):
-    dt = [('weight', float)]
-    distance_matrix_as_weight = np.matrix(distance_matrix, dtype=dt)
-    graph = nx.from_numpy_matrix(distance_matrix_as_weight)
-
-    edge_weights = []
-    start_values = np.linspace(start=0.0, stop=0.9, num=10, endpoint=True)
-    stop_values = np.linspace(start=0.1, stop=1.0, num=10, endpoint=True)
-
-    # discretize edges
-    for start_value, stop_value in zip(start_values, stop_values):
-        edges = [(u, v) for (u, v, d) in graph.edges(data=True) if
-                 (start_value < d['weight'] <= stop_value)]
-        edge_weights.append(edges)
-
-    # position of nodes
-    if pos is None:
-        pos = nx.spring_layout(graph)
-
-    fig = plt.figure()
-    fig.suptitle(title, fontsize=16)
-    plt.subplot(121)
-    # draw nodes
-    nx.draw_networkx_nodes(graph, pos)
-
-    # draw edges with corresponding weights
-    for i, edges in enumerate(edge_weights):
-        nx.draw_networkx_edges(graph, pos, edgelist=edges, width=(i/2+1))
-
-    # draw labels
-    nx.draw_networkx_labels(graph, pos, font_size=10, font_family='sans-serif')
-
-    # visualize distance matrix as heat plot
-    plt.subplot(122)
-    plt.imshow(distance_matrix, cmap='hot', interpolation='nearest')
-    plt.show()
-
-    return edge_weights, pos
-
-
-def create_env_and_schedule():
+def create_simple_env_and_schedule():
     test_parameters = ExperimentParameters(experiment_id=0, grid_id=0,
                                            number_of_agents=10, width=30,
                                            height=30, flatland_seed_value=12, asp_seed_value=94, max_num_cities=20,
@@ -134,17 +33,17 @@ def create_env_and_schedule():
     return schedule_result, schedule_trainruns
 
 
-def test_encounter_graph():
-    schedule_result, schedule_trainruns = create_env_and_schedule()
+def test_simple_env_encounter_graph():
+    schedule_result, schedule_trainruns = create_simple_env_and_schedule()
     train_schedule_dict = convert_trainrundict_to_entering_positions_for_all_timesteps(schedule_trainruns)
-    distance_matrix = compute_distance_matrix(schedule_trainruns, train_schedule_dict)
-    plot_encounter_graph(distance_matrix)
+    distance_matrix = compute_undirected_distance_matrix(schedule_trainruns, train_schedule_dict)
+    plot_encounter_graph_undirected(distance_matrix)
 
     print("distance matrix")
     print(distance_matrix)
 
 
-def test_encounter_graph_sample_12():
+def test_encounter_graph_samples_hypothesis_one():
     data_folder = './../rsp/exp_hypothesis_one_2020_03_06T21_42_54'
 
     experiment_results_list: List[ExperimentResultsAnalysis] = load_and_expand_experiment_results_from_folder(
@@ -157,12 +56,20 @@ def test_encounter_graph_sample_12():
     train_schedule_dict_full_after_malfunction = convert_trainrundict_to_entering_positions_for_all_timesteps(
         trainrun_dict_full_after_malfunction)
 
-    distance_matrix_full = compute_distance_matrix(trainrun_dict_full, train_schedule_dict_full)
-    distance_matrix_full_after_malfunction = compute_distance_matrix(trainrun_dict_full_after_malfunction,
-                                                                     train_schedule_dict_full_after_malfunction)
+    distance_matrix_full = compute_undirected_distance_matrix(trainrun_dict_full, train_schedule_dict_full)
+    distance_matrix_full_after_malfunction = compute_undirected_distance_matrix(
+        trainrun_dict_full_after_malfunction,
+        train_schedule_dict_full_after_malfunction)
     distance_matrix_diff = np.abs(distance_matrix_full_after_malfunction - distance_matrix_full)
 
-    edge_weights_full, pos = plot_encounter_graph(distance_matrix_full, "before malfunction")
-    edge_weights_full_after_malfunction, pos = plot_encounter_graph(distance_matrix_full_after_malfunction,
-                                                                    "after malfunction", pos=pos)
-    edge_weights_diff, pos = plot_encounter_graph(distance_matrix_diff, "diff", pos=pos)
+    edge_weights_full, pos = plot_encounter_graph_undirected(
+        distance_matrix_full,
+        "before_malfunction")
+    edge_weights_full_after_malfunction, pos = plot_encounter_graph_undirected(
+        distance_matrix_full_after_malfunction,
+        "after_malfunction",
+        pos=pos)
+    edge_weights_diff, pos = plot_encounter_graph_undirected(
+        distance_matrix_diff,
+        "diff_malfunction",
+        pos=pos)
