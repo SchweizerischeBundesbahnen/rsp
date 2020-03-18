f3874ac01a7efad1d7bc2c23d89f20ca9023a9ea
u214892 <u214892@sbb.ch>
2020-03-18 16:34:23 +0100

    SIM-351 cleanup upon Erik's remarks

--- rsp/hypothesis_one_compare_runs.py
+++ rsp/hypothesis_one_compare_runs.py
@@ -3,7 +3,7 @@ from rsp.hypothesis_testing.compare_runtimes import compare_runtimes
 if __name__ == '__main__':
     # allow for non-matching pkl files
     # this should be safe here since we only consider solve times and solution costs
-    COMPATIBILITY_MODE = True
+    COMPATIBILITY_MODE = False
     compare_runtimes(
         data_folder1='./exp_hypothesis_one_2020_03_04T19_19_00',
         data_folder2='./exp_hypothesis_one_2020_03_10T22_10_19',
