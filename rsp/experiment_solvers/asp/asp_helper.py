5e0b287899ba59de8a96a59875d1d20218f994aa
u214892 <u214892@sbb.ch>
2020-05-14 09:51:14 +0200

    SIM-505 revert to no_optimize=True in scheduling

--- rsp/experiment_solvers/asp/asp_helper.py
+++ rsp/experiment_solvers/asp/asp_helper.py
@@ -13,6 +13,7 @@ import numpy as np
 from importlib_resources import path
 
 from rsp.experiment_solvers.asp import theory
+from rsp.logger import rsp_logger
 
 
 class ASPObjective(Enum):
@@ -151,6 +152,8 @@ def _asp_helper(encoding_files: List[str],
     # (https://www.cs.uni-potsdam.de/~torsten/hybris.pdf  Listing 1.8 line 9)
     # bezieht sich auf eine sehr alte clingo[DL] version. Im Rahmen einer einheitlichen API für alle clingo Erweiterungen
     # (clingo[DL], clingcon, clingo[LP]) ist die neue Variante mit der python theory zu verwenden.
+    rsp_logger.info(f"no_optimize={no_optimize}")
+
     dl = theory.Theory("clingodl", "clingo-dl")
     dl.configure_propagator("propagate", "partial")
     ctl_args = [f"-c use_decided=1", f"-t{nb_threads}", "--lookahead=no"]
