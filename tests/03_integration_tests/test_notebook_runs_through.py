84f9bd1e673264f2962f497bc394c4b8967500b6
u214892 <u214892@sbb.ch>
2020-06-12 17:45:04 +0200

    SIM-545 post-merge fixes

--- tests/03_integration_tests/test_notebook_runs_through.py
+++ tests/03_integration_tests/test_notebook_runs_through.py
@@ -36,9 +36,6 @@ if __name__ == '__main__':
             dest_text = re.sub('^(plot_route_dag.*)\\)', r'\g<1>, save=True)', dest_text, flags=re.MULTILINE)
             # tweak 3: do not show Video
             dest_text = re.sub('^Video', r'#Video', dest_text, flags=re.MULTILINE)
-            # tweak 4: skip disturbance_propagation_graph_visualization which uses matplotlib window?
-            # TODO SIM-545 have a closer look what happens here. Does it still not with Eriks pr?
-            dest_text = re.sub('^(.*disturbance_propagation_graph_visualization)', r'#\g<1>', dest_text, flags=re.MULTILINE)
 
             print(dest_text)
             multiline_eval(dest_text)
