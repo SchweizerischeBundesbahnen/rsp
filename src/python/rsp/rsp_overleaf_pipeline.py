d758e949069230e1c50c97efc9186aca86461889
Erik Nygren <erik.nygren@sbb.ch>
2020-12-21 16:53:29 +0100

    updated parameters for new tests.

--- src/python/rsp/rsp_overleaf_pipeline.py
+++ src/python/rsp/rsp_overleaf_pipeline.py
@@ -6,26 +6,26 @@ from rsp.step_01_agenda_expansion.experiment_parameters_and_ranges import ReSche
 from rsp.step_01_agenda_expansion.experiment_parameters_and_ranges import ScheduleParametersRange
 
 INFRA_PARAMETERS_RANGE = InfrastructureParametersRange(
-    number_of_agents=[50, 100, 4],
+    number_of_agents=[50, 120, 4],
     width=[120, 120, 1],
     height=[120, 120, 1],
     flatland_seed_value=[190, 190, 1],
-    max_num_cities=[5, 20, 4],
-    max_rail_in_city=[2, 2, 1],
-    max_rail_between_cities=[1, 1, 1],
+    max_num_cities=[6, 20, 4],
+    max_rail_in_city=[3, 3, 1],  # up from 2
+    max_rail_between_cities=[2, 2, 1],  # up from 1
     number_of_shortest_paths_per_agent=[10, 10, 1],
 )
-SCHEDULE_PARAMETERS_RANGE = ScheduleParametersRange(asp_seed_value=[814, 814, 1], number_of_shortest_paths_per_agent_schedule=[1, 1, 1],)
+SCHEDULE_PARAMETERS_RANGE = ScheduleParametersRange(asp_seed_value=[814, 814, 1], number_of_shortest_paths_per_agent_schedule=[1, 1, 1], )
 RESCHEDULE_PARAMETERS_RANGE = ReScheduleParametersRange(
-    earliest_malfunction=[30, 30, 1],
+    earliest_malfunction=[10, 10, 1],
     malfunction_duration=[50, 50, 1],
     # take all agents (200 is larger than largest number of agents)
     malfunction_agent_id=[0, 200, 200],
-    number_of_shortest_paths_per_agent=[10, 10, 1],
+    number_of_shortest_paths_per_agent=[5, 5, 1],  # down from 10
     max_window_size_from_earliest=[60, 60, 1],
     asp_seed_value=[99, 99, 1],
     # route change is penalized the same as 30 seconds delay
-    weight_route_change=[30, 30, 1],
+    weight_route_change=[1, 1, 1], # down from 30
     weight_lateness_seconds=[1, 1, 1],
 )
 
